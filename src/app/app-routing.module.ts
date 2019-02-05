79f9c0d635f12fb51d9969f714600c63b422b3ff
Andrei <andrey.laptev@outlook.com>
2019-02-05 13:56:18 -0500

    New project creation - components & service built

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+import { FlightsComponent } from 'src/app/flights/flights.component';
+import { OrdersComponent } from 'src/app/orders/orders.component';
+import { TicketsComponent } from 'src/app/tickets/tickets.component';
+import { FavoritesComponent } from 'src/app/favorites/favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'flights',
+    component: FlightsComponent
+  },
+  {
+    path: 'orders',
+    component: OrdersComponent
+  },
+  {
+    path: 'tickets',
+    component: TicketsComponent
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FlightsComponent } from './flights/flights.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FlightsComponent,
+    CustomersComponent,
+    OrdersComponent,
+    TicketsComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
