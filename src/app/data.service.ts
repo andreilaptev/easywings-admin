d8d231fcd30e2f5187809b48073a79fe3728597b
Andrei <andrey.laptev@outlook.com>
2019-02-05 14:40:23 -0500

    CRUD functions added to data.service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllFlights() {
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/flights');
+  }
+
+  getFlight(flightId) {
+   // console.log(this.flight$)
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/flights/'+ flightId)
+  }
+
+  getAllCustomers() {
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/customers');
+  }
+
+  getCustomer(customerId) {
+   // console.log(this.flight$)
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/customers/'+ customerId)
+  }
+
+  getAllOrders() {
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/orders');
+  }
+
+  getOrder(orderId) {
+   // console.log(this.flight$)
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/orders/'+ orderId)
+  }
+
+  getAllTickets() {
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/tickets');
+  }
+
+  getTicket(ticketId) {
+   // console.log(this.flight$)
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/tickets/'+ ticketId)
+  }
+
+  getAllFavorites() {
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/favorites');
+  }
+
+  getFavorite(favoriteId) {
+   // console.log(this.flight$)
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/flifavoritesghts/'+ favoriteId)
+  }
+
 }
