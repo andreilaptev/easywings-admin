234983f4f855e8eeb44c604ce130f9e8e2335dac
Andrei <andrey.laptev@outlook.com>
2019-02-09 16:47:03 -0500

    Tickets Maintenance page created

--- src/app/tickets/tickets.component.ts
+++ src/app/tickets/tickets.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { Data } from '@angular/router/src/config';
 
 @Component({
   selector: 'app-tickets',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketsComponent implements OnInit {
 
-  constructor() { }
+tickets$: any;
+ticket: any;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+
+    this.data.getAllTickets().subscribe(
+      data => {
+        this.tickets$ = data;
+        console.log(this.tickets$);        
+      })  
   }
 
 }
